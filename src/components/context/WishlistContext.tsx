d08deb902c04f46994e7fc2cdd3cb41e3044876f
xblbong <sblhh.m@gmail.com>
2026-05-13 10:56:09 +0700

    feat: define WishlistContextType interface for managing wishlist state

--- src/components/context/WishlistContext.tsx
+++ src/components/context/WishlistContext.tsx
@@ -1,2 +1,13 @@
 "use client";
 
+import { Game } from "../types/game";
+
+interface WishlistContextType {
+  wishlist: Game[];
+  wishlistIds: number[];
+  addToWishlist: (id: number) => void;
+  removeFromWishlist: (id: number) => void;
+  toggleWishlist: (id: number) => void;
+  isInWishlist: (id: number) => boolean;
+  isHydrated: boolean; // Memberitahu komponen jika data localStorage sudah siap
+}
